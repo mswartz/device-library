03b11355ac2b6586a4699e764595bf2b4a3233b0
Mike Swartz <mswartz@gmail.com>
2014-01-22 08:09:24 -0500

    got modals working right

--- device-library/device-library.js
+++ device-library/device-library.js
@@ -1,17 +1,22 @@
 Devices = new Meteor.Collection("devices");
 
+var Modal =  function(device_selected){
+  if(device_selected===undefined){
+    $('.modal').removeClass('open');
+    $('.modal').addClass('closed');
+  } else {
+    $('.modal').removeClass('closed');
+    $('.modal').addClass('open');  
+  }
+}
+
 if (Meteor.isClient) {
+
+  Session.setDefault('device_selected', undefined);
+
   Template.hello.greeting = function () {
     return "Welcome to device-library.";
   };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
 
   Template.device_list.helpers({
     'devices' : function() {
@@ -20,7 +25,8 @@ if (Meteor.isClient) {
   });
 
   Template.device_list.events({
-    'click .device_thumb_mod' : function() {
+    'click .open' : function() {
+      Modal(Session.get('device_selected'));
       Session.set('device_selected', this._id);
     }
   });
@@ -30,10 +36,18 @@ if (Meteor.isClient) {
       return Devices.find({_id: Session.get('device_selected')}).fetch();
     }
   });
+
+  Template.device_detail.events({
+    'click .close' : function() {
+      Modal(Session.get('device_selected'));
+      Session.set('device_selected', undefined);
+    }
+  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+
     // Devices.insert({
     //   img_thumb : '/img/img_thumb.jpg',
     //   img_large : '/img/img_large.jpg',
