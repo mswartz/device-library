36b2b0e64169b900960f930b09626348b281d837
Mike Swartz <mswartz@gmail.com>
2014-02-06 10:51:44 -0500

    trying this refactor again

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+Devices = new Meteor.Collection("devices");
+
 var displayChange = function(){
   if (Session.get('mode') == undefined) {
 
@@ -18,4 +20,109 @@ var displayChange = function(){
     $('.input-modal').addClass('visible');
 
   }
+}
+
+
+if (Meteor.isClient) {
+  Session.setDefault('device_selected', undefined);
+  Session.setDefault('mode', undefined);
+
+  // the nav stuff
+  Template.nav.events({
+    'click .add_device' : function(){
+      Session.set('mode', 'input');
+      displayChange();
+    },
+    'click .logo' : function(){
+      Session.set('mode', undefined);
+      displayChange();
+    }
+
+  });
+
+  // The 'homepage' list of devices 
+  Template.device_list.events({
+    'click .device-thumb-mod' : function() {
+      Session.set('device_selected', this._id);
+      Session.set('mode', 'detail');
+      displayChange();
+    }
+  });
+
+  Template.device_list.helpers({
+    'devices' : function() {
+      return Devices.find({}).fetch();
+    },
+    //using this to put a class on the home div
+    'mode' : function() {
+      if (Session.get('mode') == 'detail'){
+        return 'detail';
+      } else if (Session.get('mode') == 'input')
+      {
+        return 'input';
+      } else {
+        return 'no-mode';
+      }
+    }
+  });
+
+
+  // View a device Detail modal
+  Template.device_detail.helpers({
+    'device' : function() {
+      var devices =  Devices.find({_id: Session.get('device_selected')}).fetch();
+
+      //reverse the history array so the latest thing is on top
+      for (var i = 0; i<devices.length; i++){
+        devices[0].history.reverse();
+      }
+      
+      return devices;
+    }
+  });
+
+  Template.device_detail.events({
+    'click .close' : function() {
+      Session.set('device_selected', undefined);
+      Session.set('mode', undefined);
+      displayChange();
+    }, 
+    'click #checkout_submit' : function() {
+      var device = Devices.find({_id: Session.get('device_selected')}).fetch();
+
+      if (device[0].status == 'in'){
+        var borrower = $('#checkout_name').val();
+        Devices.update({_id: Session.get('device_selected')}, {$set: {'borrower': borrower, 'status':'out'}});
+        Devices.update({_id: Session.get('device_selected')}, {$push: {'history': {'name' : borrower, 'message' : 'checked out the device.'}}});
+        $('#checkout_submit').val('Bring it back!');
+      } else {
+        $('#checkout_submit').val('Check it out!');
+        Devices.update({_id: Session.get('device_selected')}, {$set: {'borrower': null, 'status':'in'}});
+        Devices.update({_id: Session.get('device_selected')}, {$push: {'history': {'name' : device[0].borrower, 'message' : 'brought it back.'}}});
+      }
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.methods({
+    addDevice: function(data){
+      Devices.insert({
+        img : data.device_img,
+        name : data.device_name,
+        class : data.device_class,
+        os : data.os,
+        resolution : data.res,
+        release : data.release,
+        notes : data.notes,
+        status : 'in',
+        borrower: undefined,
+        history: [{'message' : 'The device was created.'}]
+      })
+    }
+  })
+
+  Meteor.startup(function () {
+    // Devices.remove({});
+  });
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,31 @@
-### A device library thinger
-This is a small meteor app designed for tracking the devices in our testing suite. It's supposed to be useful and fun. 
+# Upstatement Gadget Lab
+This is a small meteor app designed for tracking the devices in our testing suite. It's supposed to be useful and fun.
+
+### Dependencies:
+1. You need a dev workspace with [NPM](https://npmjs.org/), Some kind of [Compass](http://compass-style.org/) compiler, and [Bower](http://bower.io/).
+
+2. You'll need [Meteor](https://www.meteor.com/) to be installed if you haven't already:
+`curl https://install.meteor.com | /bin/sh`
+
+3. Install [Meteorite](https://github.com/oortcloud/meteorite) with NPM if you don't have it already:
+`npm install -g meteorite`
+
+
+### To run locally:
+1. Clone the repo wherever you want to run it:   
+`git clone git@github.com:mswartz/device-library.git`   
+
+2. cd into the directory that was created:
+`cd device-library`
+
+3. run `bower install` to grab the Upbase framework
+
+4. run `compass compile` or `compass watch` to build the css (or fire up LiveReload or somesuch to look at it)
+
+5. now you're ready to run the app with `meteor` or `mrt`
+
+6. The app should be running at `localhost:3000`!
+
 
 # Todo:
 #### Design
